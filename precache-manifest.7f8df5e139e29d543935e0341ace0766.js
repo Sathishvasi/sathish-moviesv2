b0df11d017c868f1381ff645a8298154bb2a2e72
Sathish kumar <skumarm@adobe.com>
2019-06-24 14:20:25 +0530

    Project initialised

---
+++ precache-manifest.7f8df5e139e29d543935e0341ace0766.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a871e19b57a51314010a9dc9f85e5e55",
+    "url": "/index.html"
+  },
+  {
+    "revision": "16cc79d45b4b2e1c6c7b",
+    "url": "/static/css/main.2007c551.chunk.css"
+  },
+  {
+    "revision": "9d8e596aea8e2e8ae43e",
+    "url": "/static/js/2.10186033.chunk.js"
+  },
+  {
+    "revision": "16cc79d45b4b2e1c6c7b",
+    "url": "/static/js/main.1ca19981.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "058f917ae090b7ec61d50c3c728966ef",
+    "url": "/static/media/no-record.058f917a.png"
+  }
+]);
